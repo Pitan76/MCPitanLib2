3ca554e59171ec021c60513e1588d008bf27e0a9
PTOM76 <ptms76@outlook.jp>
2022-07-15 18:50:54 +0900

    Extend

--- common/src/main/java/ml/pkom/mcpitanlib2/test/MPLTest.java
+++ common/src/main/java/ml/pkom/mcpitanlib2/test/MPLTest.java
@@ -1,11 +1,13 @@
 package ml.pkom.mcpitanlib2.test;
 
+import ml.pkom.mcpitanlib2.api.block.Block;
 import ml.pkom.mcpitanlib2.api.block.BlockSettings;
-import ml.pkom.mcpitanlib2.api.block.ExtendedBlock;
+import ml.pkom.mcpitanlib2.api.registry.Registries;
+import ml.pkom.mcpitanlib2.api.util.ExtendIdentifier;
 
 public class MPLTest {
     public static void init() {
-        ExtendedBlock block = new ExtendedBlock(new BlockSettings().hardness(1f).resistance(1f));
-
+        Block block = new Block(new BlockSettings().hardness(1f).resistance(1f));
+        Registries.registerBlock(new ExtendIdentifier("mpltest:block"), block);
     }
 }
